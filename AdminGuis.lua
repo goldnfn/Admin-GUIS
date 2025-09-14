b3cc90af76a12a22f67d3366283b0885d5a2e15a
goldn <ibryspates@gmail.com>
2025-09-14 15:36:23 -0500

    Update AdminGuis.lua

--- AdminGuis.lua
+++ AdminGuis.lua
@@ -1,42 +1,4 @@
-local a = game.ReplicatedStorage.MainEvent
-local b = {"CHECKER_1", "TeleportDetect", "OneMoreTime"}
-local c
-c =
-    hookmetamethod(
-    game,
-    "__namecall",
-    function(...)
-        local d = {...}
-        local self = d[1]
-        local e = getnamecallmethod()
-        local f = getcallingscript()
-        if e == "FireServer" and self == a and table.find(b, d[2]) then
-            return
-        end
-        if not checkcaller() and getfenv(2).crash then
-            hookfunction(
-                getfenv(2).crash,
-                function()
-                end
-            )
-        end
-        return c(...)
-    end
-)
-
-task.wait(3)
-
-local loadstring_code = "loadstring" 
-local StarterGui = game:GetService("StarterGui")
-
-StarterGui:SetCore("SendNotification", {
-    Title = "Da Hood Bypass enabled",
-    Text = "Do not rejoin this server!",
-    Duration = 10,
-    Icon = "rbxassetid://9499499294",
-})
-
-task.wait(3)
+task.wait(1)
 
 local iyCallback = Instance.new("BindableFunction")
 iyCallback.OnInvoke = function(button)
@@ -67,7 +29,7 @@ xkCallback.OnInvoke = function(button)
     end
 end
 StarterGui:SetCore("SendNotification", {
-    Title = "Nameless Adminn",
+    Title = "Nameless Admin",
     Text = "Do you want to run nameless admin?",
     Duration = 10,
     Button1 = "Yes",
