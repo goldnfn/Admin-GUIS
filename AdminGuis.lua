85d5cbc2536670f944dc47efa6c9ff4c2694543c
goldn <ibryspates@gmail.com>
2025-09-14 15:53:39 -0500

    Update AdminGuis.lua

--- AdminGuis.lua
+++ AdminGuis.lua
@@ -1,39 +1,48 @@
-task.wait(3)
+local loadstring_code = "loadstring" 
+local StarterGui = game:GetService("StarterGui")
 
-local iyCallback = Instance.new("BindableFunction")
+local iyCallback = Instance.new('BindableFunction')
 iyCallback.OnInvoke = function(button)
-    if button == "Yes" then
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/goldnfn/yield/refs/heads/main/yield.lua'))()
+    if button == 'Yes' then
+        loadstring(
+            game:HttpGet(
+                'https://raw.githubusercontent.com/goldnfn/yield/refs/heads/main/yield.lua'
+            )
+        )()
     else
         return
     end
 end
-StarterGui:SetCore("SendNotification", {
-    Title = "Infinite Yield",
-    Text = "Do you want to run Infinite Yield?",
+StarterGui:SetCore('SendNotification', {
+    Title = 'Infinite Yield',
+    Text = 'Do you want to run Infinite Yield?',
     Duration = 10,
-    Button1 = "Yes",
-    Button2 = "No",
-    Icon = "rbxassetid://126864685454351"",
-    Callback = iyCallback
+    Button1 = 'Yes',
+    Button2 = 'No',
+    Icon = 'rbxassetid://126864685454351',
+    Callback = iyCallback,
 })
 
 task.wait(5)
 
-local xkCallback = Instance.new("BindableFunction")
+local xkCallback = Instance.new('BindableFunction')
 xkCallback.OnInvoke = function(button)
-    if button == "Yes" then
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source'))()
+    if button == 'Yes' then
+        loadstring(
+            game:HttpGet(
+                'https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source'
+            )
+        )()
     else
         return
     end
 end
-StarterGui:SetCore("SendNotification", {
-    Title = "Nameless Admin",
-    Text = "Do you want to run nameless admin?",
+StarterGui:SetCore('SendNotification', {
+    Title = 'Nameless Admin',
+    Text = 'Do you want to run nameless admin?',
     Duration = 10,
-    Button1 = "Yes",
-    Button2 = "No",
-    Icon = "rbxassetid://126864685454351"",
-    Callback = xkCallback
+    Button1 = 'Yes',
+    Button2 = 'No',
+    Icon = 'rbxassetid://126864685454351',
+    Callback = xkCallback,
 })
